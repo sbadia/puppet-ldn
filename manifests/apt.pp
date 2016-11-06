e3d04985fa5f91186168e34e6a1aa6f838f72d9c
Sebastien Badia <seb@sebian.fr>
2016-11-06 14:55:01 +0100

    apt: Migrate to puppetlabs/apt 2.x

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -12,32 +12,27 @@ class public::apt {
   $downcase_lsbdistid = downcase($::lsbdistid)
 
   apt::source { "${downcase_lsbdistid}_${::lsbdistcodename}":
-    location    => "http://ftp.fr.debian.org/${downcase_lsbdistid}/",
-    release     => $::lsbdistcodename,
-    repos       => 'main contrib non-free',
-    include_src => true,
+    location => "http://ftp.fr.debian.org/${downcase_lsbdistid}/",
+    release  => $::lsbdistcodename,
+    repos    => 'main contrib non-free',
   }
 
   apt::source { "${downcase_lsbdistid}_${::lsbdistcodename}_security":
-    location          => 'http://security.debian.org',
-    release           => "${::lsbdistcodename}/updates",
-    repos             => 'main',
-    required_packages => 'debian-keyring debian-archive-keyring',
-    include_src       => true,
+    location => 'http://security.debian.org',
+    release  => "${::lsbdistcodename}/updates",
+    repos    => 'main',
   }
 
   apt::source { "${downcase_lsbdistid}_${::lsbdistcodename}_updates":
-    location    => "http://ftp.fr.debian.org/${downcase_lsbdistid}/",
-    release     => "${::lsbdistcodename}-updates",
-    repos       => 'main',
-    include_src => true,
+    location => "http://ftp.fr.debian.org/${downcase_lsbdistid}/",
+    release  => "${::lsbdistcodename}-updates",
+    repos    => 'main',
   }
 
   apt::source { "${downcase_lsbdistid}_${::lsbdistcodename}_backports":
-    location    => "http://ftp.fr.debian.org/${downcase_lsbdistid}/",
-    release     => "${::lsbdistcodename}-backports",
-    repos       => 'main',
-    include_src => true,
+    location => "http://ftp.fr.debian.org/${downcase_lsbdistid}/",
+    release  => "${::lsbdistcodename}-backports",
+    repos    => 'main',
   }
 
 }
